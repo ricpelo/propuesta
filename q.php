f0d44588aaa5dd8076e82a6afe2b8c9448998347
Ricardo Pérez <ricpelo@gmail.com>
2017-11-24 12:50:07 +0100

    Genera Markdown y cambia el .xlsx

--- q.php
+++ q.php
@@ -10,18 +10,26 @@ $highestRow = $objWorksheet->getHighestDataRow(); // e.g. 10
 $highestColumn = $objWorksheet->getHighestDataColumn(); // e.g 'F'
 $highestColumnIndex = PHPExcel_Cell::columnIndexFromString($highestColumn); // e.g. 5
 
-echo '<table>' . "\n";
-for ($row = 1; $row <= $highestRow; $row++) {
-    echo '<tr>' . PHP_EOL;
-    for ($col = 0; $col < $highestColumnIndex; $col++) {
-        echo '<td>' . 
-             $objWorksheet->getCellByColumnAndRow($col, $row)
-                 ->getValue() . 
-             '</td>' . PHP_EOL;
-    }
-    echo '</tr>' . PHP_EOL;
-    if ($row > 1) $objWorksheet->getCellByColumnAndRow($col - 1, $row)->setValue('hola');
+for ($row = 2; $row <= $highestRow; $row++) {
+    $codigo      = $objWorksheet->getCell("A$row")->getValue();
+    $corta       = $objWorksheet->getCell("B$row")->getValue();
+    $larga       = $objWorksheet->getCell("C$row")->getValue();
+    $prioridad   = $objWorksheet->getCell("D$row")->getValue();
+    $tipo        = $objWorksheet->getCell("E$row")->getValue();
+    $complejidad = $objWorksheet->getCell("F$row")->getValue();
+    $entrega     = $objWorksheet->getCell("G$row")->getValue();
+
+    echo "| **$codigo** | **$corta**   |" . PHP_EOL;
+    echo "| ----------: | :----------- |" . PHP_EOL;
+    echo "| Descripción | $larga       |" . PHP_EOL;
+    echo "| Prioridad   | $prioridad   |" . PHP_EOL;
+    echo "| Tipo        | $tipo        |" . PHP_EOL;
+    echo "| Complejidad | $complejidad |" . PHP_EOL;
+    echo "| Entrega     | $entrega     |" . PHP_EOL;
+    echo "\n[]()\n\n";
+
+    $objWorksheet->getCellByColumnAndRow(7, $row)->setValue('hola');
 }
-echo '</table>' . PHP_EOL;
+
 $writer = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
 $writer->save('Requisitos2.xlsx');
