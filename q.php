181476464345af8b2c88b53ad3f94b2849533632
Ricardo Pérez <ricpelo@gmail.com>
2017-11-24 12:33:11 +0100

    Funciona mejor con archivos .xlsx

--- q.php
+++ q.php
@@ -3,18 +3,17 @@
 
 require 'vendor/autoload.php';
 
-$objPHPExcel = PHPExcel_IOFactory::load("Requisitos.ods");
-
-$objWorksheet = $objPHPExcel->getActiveSheet();
-// Get the highest row and column numbers referenced in the worksheet
-$highestRow = $objWorksheet->getHighestRow(); // e.g. 10
-$highestColumn = $objWorksheet->getHighestColumn(); // e.g 'F'
+PHPExcel_Settings::setLocale('es');
+$objPHPExcel = PHPExcel_IOFactory::load("Requisitos.xlsx");
+$objWorksheet = $objPHPExcel->getSheet(0);
+$highestRow = $objWorksheet->getHighestDataRow(); // e.g. 10
+$highestColumn = $objWorksheet->getHighestDataColumn(); // e.g 'F'
 $highestColumnIndex = PHPExcel_Cell::columnIndexFromString($highestColumn); // e.g. 5
 
 echo '<table>' . "\n";
-for ($row = 1; $row <= $highestRow; ++$row) {
+for ($row = 1; $row <= $highestRow; $row++) {
     echo '<tr>' . PHP_EOL;
-    for ($col = 0; $col < $highestColumnIndex; ++$col) {
+    for ($col = 0; $col < $highestColumnIndex; $col++) {
         echo '<td>' . 
              $objWorksheet->getCellByColumnAndRow($col, $row)
                  ->getValue() . 
@@ -24,5 +23,5 @@ for ($row = 1; $row <= $highestRow; ++$row) {
     if ($row > 1) $objWorksheet->getCellByColumnAndRow($col - 1, $row)->setValue('hola');
 }
 echo '</table>' . PHP_EOL;
-$writer = PHPExcel_IOFactory::createWriter($objPHPExcel, 'OpenDocument');
-$writer->save('Requisitos.ods');
+$writer = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+$writer->save('Requisitos2.xlsx');
