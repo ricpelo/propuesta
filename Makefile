84c9ca70024a1866de20d6a957c8952b2db7bd25
Ricardo Pérez <ricpelo@gmail.com>
2017-11-24 14:51:31 +0100

    Comprueba paquetes necesarios

--- Makefile
+++ Makefile
@@ -1,6 +1,11 @@
-.PHONY: all
+.PHONY: all check req
 
-all:
+all: check req
+
+check:
+	./check-packages.sh
+
+req:
 	php requisitos.php > requisitos.md
 	pandoc -s propuesta.md requisitos.md -o requisitos.txt
 	pandoc -s --toc requisitos.txt -V lang=es -V fontfamily=mathpazo -V fontsize=11pt -o propuesta.pdf
---
+++ check-packages.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+VER="2.0.3"
+FILE="pandoc-$VER-1-amd64.deb"
+
+if ! dpkg -s pandoc > /dev/null 2>&1
+then
+    echo "Descargando e instalando Pandoc $VER..."
+    wget -q "https://github.com/jgm/pandoc/releases/download/$VER/$FILE"
+    sudo dpkg -i $FILE
+    rm $FILE
+    sudo apt -f install
+fi
+
+PKG="texlive-full"
+if ! dpkg -s $PKG > /dev/null 2>&1
+then
+    echo "Instalando $PKG (puede tardar bastante)..."
+    sudo apt install $PKG
+fi
