f6077f11c13c922556892cb11f6c819c84c72734
Ricardo Pérez <ricpelo@gmail.com>
2017-11-26 19:39:30 +0100

    Actualiza colores

---
+++ check-label.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+ghi label mínimo -c e99695
+ghi label importante -c mediumpurple
+ghi label opcional -c fef2c0
+ghi label fácil -c f9ca98
+ghi label media -c 93d8d7
+ghi label difícil -c b60205
+ghi label funcional -c d4c5f9
+ghi label técnico -c 006b75
+ghi label información -c 0052cc
