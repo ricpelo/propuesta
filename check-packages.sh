2fb4e54d3b149b68d78560264a4125cb4d2e51af
Ricardo Pérez <ricpelo@gmail.com>
2017-12-16 15:43:25 +0100

    Actualiza a Pandoc 2.0.5

--- check-packages.sh
+++ check-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VER="2.0.3"
+VER="2.0.5"
 FILE="pandoc-$VER-1-amd64.deb"
 
 if ! dpkg -s pandoc > /dev/null 2>&1
@@ -8,7 +8,7 @@ then
     echo "Descargando e instalando Pandoc $VER..."
     wget -q "https://github.com/jgm/pandoc/releases/download/$VER/$FILE"
     sudo dpkg -i $FILE
-    rm $FILE
+    rm -f $FILE
     sudo apt -f install
 fi
 
