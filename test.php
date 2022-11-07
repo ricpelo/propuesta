ecc65ef5294a31bfb644f08610a7031dab593ff5
Ricardo Pérez <ricpelo@gmail.com>
2022-11-07 12:18:44 +0100

    Mejora

--- test.php
+++ test.php
@@ -6,18 +6,19 @@ $client = new \Github\Client();
 $token = getenv('GITHUB_TOKEN');
 $client->authenticate($token, null, Github\AuthMethod::ACCESS_TOKEN);
 $login = $client->currentUser()->show()['login'];
-$repo = 'propuesta';
-$project_number = 3;
+$repo = $argv[2]; // 'propuesta';
 
-$query = <<<EOT
-query {
-    user(login: "$login") {
-        projectV2(number: $project_number) {
-            id
-        }
-    }
-}
-EOT;
+// $project_number = 3;
+
+// $query = <<<EOT
+// query {
+//     user(login: "$login") {
+//         projectV2(number: $project_number) {
+//             id
+//         }
+//     }
+// }
+// EOT;
 
 $query = <<<EOT
 query{
@@ -25,6 +26,8 @@ query{
         projectsV2(first: 20) {
             nodes {
                 id
+                number
+                public
                 title
             }
         }
@@ -33,45 +36,74 @@ query{
 EOT;
 
 $result = $client->api('graphql')->execute($query);
-var_dump($result); die();
 
-$result = $client->api('graphql')->execute($query);
-$project_id = $result['data']['user']['projectV2']['id'];
-var_dump($project_id);
+if (!isset($result['data']['user']['projectsV2'])
+    || !isset($result['data']['user']['projectsV2']['nodes'])
+    || empty($result['data']['user']['projectsV2']['nodes'])) {
+    echo "No se han encontrado proyectos en la cuenta del usuario.\n";
+    exit(1);
+}
+
+$projects = $result['data']['user']['projectsV2']['nodes'];
+
+while (true) {
+    echo "Proyectos en la cuenta del usuario:\n";
+
+    foreach ($projects as $k => $project) {
+        echo "$k: #{$project['number']} {$project['title']}\n";
+    }
+
+    echo "Seleccione uno: ";
+    $project_number = '';
+    fscanf(STDIN, '%s', $project_number);
+    if (!isset($projects[$project_number])) {
+        echo "Error: opción incorrecta.\n";
+    } else {
+        $project_id = $projects[$project_number]['id'];
+        break;
+    }
+}
+
+echo "La opción elegida es $project_number\n";
+echo "El ID del proyecto es $project_id\n";
+
+#$result = $client->api('graphql')->execute($query);
+#$project_id = $result['data']['user']['projectV2']['id'];
+#var_dump($project_id);
 
 $query = <<<EOT
 query{
     node(id: "$project_id") {
-      ... on ProjectV2 {
-        fields(first: 20) {
-          nodes {
-            ... on ProjectV2Field {
-              id
-              name
-            }
-            ... on ProjectV2IterationField {
-              id
-              name
-              configuration {
-                iterations {
-                  startDate
-                  id
+        ... on ProjectV2 {
+            fields(first: 20) {
+                nodes {
+                    ... on ProjectV2Field {
+                        id
+                        name
+                    }
+                    ... on ProjectV2IterationField {
+                        id
+                        name
+                        configuration {
+                            iterations {
+                                startDate
+                                id
+                            }
+                        }
+                    }
+                    ... on ProjectV2SingleSelectField {
+                        id
+                        name
+                        options {
+                            id
+                            name
+                        }
+                    }
                 }
-              }
             }
-            ... on ProjectV2SingleSelectField {
-              id
-              name
-              options {
-                id
-                name
-              }
-            }
-          }
         }
-      }
     }
-  }
+}
 EOT;
 
 $result = $client->api('graphql')->execute($query);
@@ -89,8 +121,10 @@ foreach ($result['data']['node']['fields']['nodes'] as $node) {
     }
 }
 
-var_dump($status_id);
-var_dump($todo_id);
+if (!isset($status_id) || !isset($todo_id)) {
+    echo "Error: El proyecto no tiene un Status: Todo\n";
+    exit(1);
+}
 
 $query = <<<EOT
 query FindRepo {
@@ -101,8 +135,14 @@ query FindRepo {
 EOT;
 
 $result = $client->api('graphql')->execute($query);
+
+if (!isset($result['data']['repository'])
+    || empty($result['data']['repository'])) {
+    echo "Error: no se ha encontrado el repositorio.\n";
+    exit(1);
+}
+
 $repo_id = $result['data']['repository']['id'];
-var_dump($repo_id);
 
 $query = <<<EOT
 mutation CreateIssue {
@@ -121,7 +161,14 @@ mutation CreateIssue {
 EOT;
 
 $result = $client->api('graphql')->execute($query);
-var_dump($result);
+
+if (!isset($result['data']['createIssue'])
+    || !isset($result['data']['createIssue']['issue'])
+    || empty($result['data']['createIssue']['issue'])) {
+    echo "Error: no se ha podido crear la incidencia.\n";
+    exit(1);
+}
+
 $issue_id = $result['data']['createIssue']['issue']['id'];
 
 $query = <<<EOT
@@ -135,7 +182,14 @@ mutation {
 EOT;
 
 $result = $client->api('graphql')->execute($query);
-var_dump($result);
+
+if (!isset($result['data']['addProjectV2ItemById'])
+    || !isset($result['data']['addProjectV2ItemById']['item'])
+    || empty($result['data']['addProjectV2ItemById']['item'])) {
+    echo "Error: no se ha podido asignar la incidencia al proyecto.\n";
+    exit(1);
+}
+
 $item_id = $result['data']['addProjectV2ItemById']['item']['id'];
 
 $query = <<<EOT
@@ -158,4 +212,11 @@ mutation {
 EOT;
 
 $result = $client->api('graphql')->execute($query);
-var_dump($result);
+// $result['data']['updateProjectV2ItemFieldValue']['projectV2Item']['id']
+
+if (!isset($result['data']['updateProjectV2ItemFieldValue'])
+    || !isset($result['data']['updateProjectV2ItemFieldValue']['projectV2Item'])
+    || empty($result['data']['updateProjectV2ItemFieldValue']['projectV2Item'])) {
+    echo "Error: no se ha podido poner el Status: Todo a la incidencia en el proyecto.\n";
+    exit(1);
+}
